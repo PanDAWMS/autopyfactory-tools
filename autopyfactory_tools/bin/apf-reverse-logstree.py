cf2f2122bc4d091d44fbe383e87722576aa9620e
Jose Caballero <jcaballero@bnl.gov>
2016-07-04 13:20:39 -0400

    passing input options to script apf-reverse-logstree

--- autopyfactory_tools/bin/apf-reverse-logstree.py
+++ autopyfactory_tools/bin/apf-reverse-logstree.py
@@ -1,43 +1,57 @@
 #!/usr/bin/env python
 
+import commands
+import getopt
 import os
 import re
-import commands
+import sys
 
 # FIXME
-#
-#  * BASEDIR and NEWBASEDIR should be read from configfile
-#
 #  * consider to specify a list of queues and/or list of dates
 #    instead of everything
 #
 
 
+# DEFAULTS
 BASEDIR = '/home/apf/factory/logs'
-NEWBASEDIR = '/tmp/l1'
+NEWDIR = '/tmp/apf-reverse-logstree'
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "", ["basedir=", "newdir="])
+except getopt.GetoptError, err:
+    print str(err)
+
+for k,v in opts:
+    if k == "--basedir":
+    BASEDIR=v 
+    if k == "--newdir":
+    NEWDIR=v 
+
+print BASEDIR
+print NEWDIR
 
 class QUEUE(object):
-        def __init__(self, queue):
-                self.queue = queue
-                self.dates = []
+    def __init__(self, queue):
+        self.queue = queue
+        self.dates = []
 
 LIST_QUEUES = {}
 
 RE = re.compile(r"(\d{4})-(\d{2})-(\d{2})?$")
 
 for i in os.listdir(BASEDIR):
-        if RE.match(i):
-                DATE = i
-                for queue in os.listdir('%s/%s' %(BASEDIR,DATE)):
-
-                        if queue not in LIST_QUEUES.keys():
-                                LIST_QUEUES[queue] = QUEUE(queue)
-
-                        LIST_QUEUES[queue].dates.append(DATE)
+    if RE.match(i):
+        DATE = i
+        for queue in os.listdir('%s/%s' %(BASEDIR,DATE)):
+            if queue not in LIST_QUEUES.keys():
+                LIST_QUEUES[queue] = QUEUE(queue)
+            LIST_QUEUES[queue].dates.append(DATE)
 
 
 for q,Q in LIST_QUEUES.iteritems():
-        commands.getoutput('mkdir %s/%s' %(NEWBASEDIR, q) )
-        for DATE in Q.dates:
-                commands.getoutput('ln -s %s/%s/%s %s/%s/%s' %(BASEDIR, DATE , q ,NEWBASEDIR, q, DATE) )
-                #print ('ln -s %s/%s/%s %s/%s/%s' %(BASEDIR, DATE , q ,NEWBASEDIR, q, DATE) )
+    cmd = 'mkdir -p %s/%s' %(NEWDIR, q) 
+    commands.getoutput( cmd )
+    for DATE in Q.dates:
+        cmd = 'ln -s %s/%s/%s %s/%s/%s' %(BASEDIR, DATE , q ,NEWDIR, q, DATE) 
+        commands.getoutput(cmd)
+        #print (cmd)
